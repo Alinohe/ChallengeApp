aaa05763c572ab19305bdf3f595179a72cd78111
Robert Domanski <robertdomanski12@gmail.com>
2024-12-08 18:36:38 +0000

    pioerwsze zadanie

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Ewa";
+int age = 28;
+string sex = "female";
+
+
+if (sex == "female" && age < 30)
+{
+
+        Console.WriteLine("Jestem Ewa i mam mniej niz 30 lat");
+  
+
+}
+if (name == "Ewa" && age==30)
+{
+    Console.WriteLine("Jestem Ewa i mam 30 lat");
+}
+if (sex  == "male" && age <18)
+{
+    Console.WriteLine("niepelnoletni mezczyzna");
+}
