b7da7cee604dfca620f84d78225f944374f6dcfc
Robert Domanski <robertdomanski12@gmail.com>
2024-12-10 21:06:56 +0000

    dzien5 zadanie domowe

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -1,20 +1,75 @@
-﻿string name = "Ewa";
-int age = 28;
-string sex = "female";
+﻿using System.Xml;
 
+int number = 4566;
+string numberInString = number.ToString();
+char[] letters = numberInString.ToArray();
+Console.WriteLine("Wyniki dla liczby" + number);
 
-if (sex == "female" && age < 30)
+int c0 = 0;
+int c1 = 0;
+int c2 = 0;
+int c3 = 0;
+int c4 = 0;
+int c5 = 0;
+int c6 = 0;
+int c7 = 0;
+int c8 = 0;
+int c9 = 0;
+
+foreach (char c in letters)
 {
+    if (c == '0')
+    {
+        c0++;
+    }
+    else if (c == '1')
+    {
+        c1++;
+    }
+    else if (c == '2')
+    {
+        c2++;
+    }
+    else if (c == '3')
+    {
+        c3++;
+    }
+    else if (c == '4')
+    {
+        c4++;
+    }
+    else if (c == '5')
+    {
+        c5++;
+    }
+    else if (c == '6')
+    {
+        c6++;
+    }
+    else if (c == '7')
+    {
+        c7++;
+    }
+    else if (c == '8')
+    {
+        c8++;
+    }
+    else if (c == '9')
+    {
+        c9++;
+    }
 
-        Console.WriteLine("Jestem Ewa i mam mniej niz 30 lat");
-  
 
 }
-if (name == "Ewa" && age==30)
-{
-    Console.WriteLine("Jestem Ewa i mam 30 lat");
-}
-if (sex  == "male" && age <18)
-{
-    Console.WriteLine("niepelnoletni mezczyzna");
-}
+Console.WriteLine("0=>" + c0);
+Console.WriteLine("1=>" + c1);
+Console.WriteLine("2=>" + c2);
+Console.WriteLine("3=>" + c3);
+Console.WriteLine("4=>" + c4);
+Console.WriteLine("5=>" + c5);
+Console.WriteLine("6=>" + c6);
+Console.WriteLine("7=>" + c7);
+Console.WriteLine("8=>" + c8);
+Console.WriteLine("9=>" + c9);
+
+
